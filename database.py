cf9e12f928d9e0cd6d31d8917e1bad5f2bec5aa3
Hector Romero <sphere367@gmail.com>
2024-10-31 01:04:22 -0600

    /bugfix se removieron todos los pycaches

--- database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "mysql+aiomysql://root:p4$$w0rd@localhost:3306/jimbo"
-
-async_engine = create_async_engine(DATABASE_URL, future=True, echo=True)
-AsyncSessionLocal = sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False)
-Base = declarative_base()
